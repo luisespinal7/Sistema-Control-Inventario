f6e424bf9dd1a9d271cc9cd772f6f0d375c3734a
luisespinal7 <48111267+luisespinal7@users.noreply.github.com>
2021-04-12 17:56:37 -0500

    Update Descuento.java

--- SistemaControlInventario/src/main/java/edu/unah/bbddI/model/Descuento.java
+++ SistemaControlInventario/src/main/java/edu/unah/bbddI/model/Descuento.java
@@ -2,9 +2,11 @@ package edu.unah.bbddI.model;
 
 import java.util.List;
 
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
 @Table(name="DESCUENTO")
